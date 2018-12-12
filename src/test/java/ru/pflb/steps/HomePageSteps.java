e28fd96738a292d012186b95f825276d9b2bdfe0
Alehandroalex <alehandrov_alex@mail.ru>
2018-12-12 17:19:34 +0300

    added new class with steps on home page yanex.ru

---
+++ src/test/java/ru/pflb/steps/HomePageSteps.java
@@ -0,0 +1,28 @@
+package ru.pflb.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.pflb.pages.HomePage;
+import ru.pflb.tech.BaseStep;
+import ru.pflb.tech.Context;
+
+public class HomePageSteps extends BaseStep {
+
+    private static final Logger LOGGER = LogManager.getLogger(HomePageSteps.class);
+
+    HomePage homePage;
+
+    public HomePageSteps(Context context){
+        super(context);
+        LOGGER.debug("HomePageSteps is created");
+        homePage = context.getPageObjectManager().getHomePage();
+    }
+
+    @Then("^should be button \"([^\"]*)\"$")
+    public void shouldBeButton(String arg0) throws Throwable{
+        homePage.getToGetMail().isDisplayed();
+    }
+
+}
