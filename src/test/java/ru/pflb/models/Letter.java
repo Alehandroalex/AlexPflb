dae67a7df9b545d9aebe5b3f27e21817a13521c0
Alehandroalex <alehandrov_alex@mail.ru>
2018-12-14 13:18:33 +0300

    created Letter class

---
+++ src/test/java/ru/pflb/models/Letter.java
@@ -0,0 +1,38 @@
+package ru.pflb.models;
+
+import java.util.List;
+
+public class Letter {
+
+    private List<String>recipientList;
+    private String topic;
+    private String body;
+
+    public List<String> getRecipientList(){
+        return recipientList;
+    }
+
+    public Letter addRecipient(String recipient){
+        this.recipientList.add(recipient);
+        return this;
+    }
+
+    public String getTopic(){
+        return topic;
+    }
+
+    public Letter setTopic(String topic){
+        this.topic = topic;
+        return this;
+    }
+
+    public String getBody(){
+        return body;
+    }
+
+    public Letter setBody(String body){
+        this.body = body;
+        return this;
+    }
+
+}
