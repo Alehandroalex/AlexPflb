9ed020b65f80b9cf887fcf6b25a17d9d6fe15edc
Alehandroalex <alehandrov_alex@mail.ru>
2019-01-22 17:03:08 +0300

    rename parameter selenium hub

--- src/test/java/ru/pflb/tech/configuration/Configuration.java
+++ src/test/java/ru/pflb/tech/configuration/Configuration.java
@@ -5,7 +5,7 @@ public class Configuration {
     public static final Browser BROWSER = Browser.getByName(System.getProperty("browser", "firefox"));
     public static final Boolean USE_SELENIUM_GRID = Boolean.parseBoolean(System.getProperty("useSeleniumGrid", "false"
     ));
-    public static final String SELENIUM_HUB_URL = System.getProperty("hub_url", "http://localhost:4444/wd/hub");
+    public static final String SELENIUM_HUB_URL = System.getProperty("seleniumHubUrl", "http://localhost:4444/wd/hub");
     public static final String URL = System.getProperty("url");
     public static final String LOGIN = System.getProperty("login");
     public static final String PASSWORD = System.getProperty("password");
